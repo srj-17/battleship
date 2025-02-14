82ac006d48eb08c31128d9674dc4c1387fb8f9a0
srj-17 <saugatrijal17@gmail.com>
2025-02-14 17:18:34 +0545

    feat: Add a bit of intelligence to the computer
    If an attack hits any of the ships, the computer
    will try to hit one of the neighboring coordinates.
    
    This will make the game more competitive for the real
    player

--- src/computerLogic.js
+++ src/computerLogic.js
@@ -1,5 +1,36 @@
+let recentlyAttackedCoordinate = null;
+let recentlyHit = false;
+
+function recordCoordinate(coordinate) {
+    recentlyAttackedCoordinate = coordinate;
+}
+
+function recordHit() {
+    recentlyHit = true;
+}
+
+function clearHit() {
+    recentlyHit = false;
+}
+
 function getAttackCoordinates() {
-    return [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+    if (recentlyHit) {
+        const [recentlyAttackedX, recentlyAttackedY] =
+            recentlyAttackedCoordinate;
+        const neighbors = [
+            [recentlyAttackedX, recentlyAttackedY + 1],
+            [recentlyAttackedX, recentlyAttackedY - 1],
+            [recentlyAttackedX + 1, recentlyAttackedY],
+            [recentlyAttackedX - 1, recentlyAttackedY],
+        ];
+
+        // select a random neighbor
+        const coordinateToAttack =
+            neighbors[Math.floor(Math.random() * neighbors.length)];
+        return coordinateToAttack;
+    } else {
+        return [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+    }
 }
 
 // takes player object as input
@@ -15,7 +46,14 @@ function attack(player) {
         if (!player.gameboard.trackingGrid[randomX][randomY]) break;
     }
 
-    player.gameboard.receiveAttack(randomCoordinate);
+    recordCoordinate(randomCoordinate);
+    console.log(recentlyHit, randomCoordinate);
+
+    // receiveAttack returns true if the attack hits the ship
+    const hitsShip = player.gameboard.receiveAttack(randomCoordinate);
+
+    if (hitsShip) recordHit();
+    else clearHit();
 }
 
 export { attack };
